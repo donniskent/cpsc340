94ac2f04f63195d794620c6eeedff9e20844c26d
Donnis Kent <dkent@SEEC.local>
2021-09-27 16:23:25 -0400

    first commit

---
+++ registration.php
@@ -0,0 +1,32 @@
+<html>
+
+<body> 
+<form name=regform> form test</form> 
+<label for=fname> First Name </label>
+<input name=fname> 
+
+
+
+</body> 
+
+ 
+<html>
+
+
+<?php 
+/* 
+User Registration
+if user already in database
+display error
+exit
+generate salt
+encrypt password
+store user data and encrypted password and salt in database
+
+*/
+
+//have to get input from form 
+	
+
+
+?>
