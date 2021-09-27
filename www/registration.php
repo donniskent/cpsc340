cdaecae5976479e56d1c0ab017b2bb7f9feb4210
Donnis Kent <dkent@SEEC.local>
2021-09-27 16:46:15 -0400

    Testing 123

--- www/registration.php
+++ www/registration.php
@@ -4,7 +4,14 @@
 <form name=registration-form> form test</form> 
 <label for=fname> First Name </label>
 <input name=fname> 
+<?php 
 
+$pdo = new PDO('mysql:host=localhost;dbname=moviesite', movie, theforce);
+
+
+
+
+?>
 
 
 </body> 
