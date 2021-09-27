a800ddcc0b701f434a44f72f67d2b337563aaaf2
Donnis Kent <dkent@SEEC.local>
2021-09-27 16:38:28 -0400

    testing

--- www/registration.php
+++ www/registration.php
@@ -1,7 +1,7 @@
 <html>
 
 <body> 
-<form name=regform> form test</form> 
+<form name=registration-form> form test</form> 
 <label for=fname> First Name </label>
 <input name=fname> 
 
@@ -11,22 +11,3 @@
 
  
 <html>
-
-
-<?php 
-/* 
-User Registration
-if user already in database
-display error
-exit
-generate salt
-encrypt password
-store user data and encrypted password and salt in database
-
-*/
-
-//have to get input from form 
-	
-
-
-?>
