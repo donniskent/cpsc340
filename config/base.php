7b4cbd8bbbea3b7c62fc75b48b4553ad0e9965a7
Donnis Kent <dkent@SEEC.local>
2021-12-05 18:57:49 -0500

    Added utilities file

--- config/base.php
+++ config/base.php
@@ -29,7 +29,7 @@ $user= $_SESSION['username'];}?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: white !important">
   <div class="container-fluid">
-    <a class="navbar-brand" href="homepage.php">MetaMovie</a>
+    <a class="navbar-brand" href="homepage.php">CineMeta</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -40,27 +40,25 @@ $user= $_SESSION['username'];}?>
         </li>
         <li class="nav-item">
           
-		  <?php if (isset($_SESSION["username"])) {
-			
+		  
+			<?php
 			if(isset($_SESSION["admin"])) {
 			echo '<a class="nav-link" href="admin.php">Admin</a>';}
-		  
-		  else {
-			
-			echo '<a class="nav-link" href="user.php?user='.$user.'">Your Page</a> ';
-
-		  }
+		  ?>
+	
 		  
 		  
-		  
-		  
-		  
-		  }
-		  
-		  
-		?>
 		
 		</li>
+		<li class="nav-item"> <?php
+		 if (isset($_SESSION["username"])) {
+		echo '<a class="nav-link" href="user.php?user='.$user.'">Your Page</a> ';
+		}
+		?>
+		
+		
+		</li>
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Utilities
